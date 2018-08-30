bc15f09f87e8eb40d619d0ca632b376b4535e3ab
anhongzhan <anhongzhan@163.com>
2018-08-31 02:18:18 +0800

    second commit

---
+++ day4.8/kernel/main.c
@@ -0,0 +1,41 @@
+#include<header.h>
+void bootmain(void){
+	int i;
+	char *p;
+
+	init_palette();//设定调色板
+
+	for(i=0xa0000;i<=0xaffff;i++){
+		p = (char*)i;		
+		*p = i & 0x0f;
+	}
+
+	clear_screen(15);//由于一进来这里显示的是"my kernel is running"等字符串，这里刷屏为灰色，去除字符串
+	
+	char* vram;
+	vram = (char*)0xa0000;
+	int xsize = 320;
+	int ysize = 200;
+
+	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize-1, ysize-29);
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize-28, xsize-1, ysize-28);
+	boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize-27, xsize-1, ysize-27);
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize-26, xsize-1, ysize-1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize-24, 59, ysize-24);
+	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize-24, 2, ysize-4);
+	boxfill8(vram, xsize, COL8_848484, 3, ysize-4, 59, ysize-4);
+	boxfill8(vram, xsize, COL8_848484, 59, ysize-23, 59, ysize-5);
+	boxfill8(vram, xsize, COL8_000000, 2, ysize-3, 59, ysize-3);
+	boxfill8(vram, xsize, COL8_000000, 60, ysize-24, 60, ysize-3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize-47, ysize-24, xsize-4, ysize-24);
+	boxfill8(vram, xsize, COL8_848484, xsize-47, ysize-23, xsize-47, ysize-4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize-47, ysize-3, xsize-4, ysize-3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize-3, ysize-24, xsize-3, ysize-3);
+
+
+	for(;;){
+		io_halt();
+	}
+}
